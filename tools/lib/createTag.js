0d51e8b279e44605b1099ea53ff4024e888e3f6d
Darren Schnare <schnare.d@gmail.com>
2017-03-18 02:43:15 -0400

    feat(createTag): Automatically set the version key in package.json when creating an new tag

--- tools/lib/createTag.js
+++ tools/lib/createTag.js
@@ -1,7 +1,23 @@
 const { exec } = require('child_process')
+const fs = require('fs')
+const path = require('path')
 const getLatestCommits = require('./lib/getLatestCommits')
 const getLatestTag = require('./lib/getLatestTag')
 const bumpVersion = require('./lib/bumpVersion')
+
+function call (cmd) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, { cwd: process.cwd() }, (error, stdout, stderr) => {
+      if (error) {
+        reject(error)
+      } else if (stderr) {
+        reject(new Error(stderr))
+      } else {
+        resolve(stdout.trim())
+      }
+    })
+  })
+}
 
 module.exports = function createTag ({ nextVersion =  null, lightweight = false } = {}) {
   return getLatestCommits().then(commits => {
@@ -19,6 +35,21 @@ module.exports = function createTag ({ nextVersion =  null, lightweight = false 
       ? Promise.resolve(nextVersion)
       : getLatestTag().then(tag => {
         return bumpVersion(tag, { semverPart })
+      }).then(nextVersion => {
+        const pkg = require(path.resolve('package.json'))
+        if (pkg.version !== nextVersion) {
+          pkg.version = nextVersion
+          const jsonText = JSON.stringify(pkg, null, 2)
+          fs.writeFileSync(path.resolve('package.json'), jsonText, 'utf8')
+          call('git add package.json').then(() => {
+            return call('git commit -m "chore(package): Bump version"')
+          }).then(() => {
+            commits.push({ subject: 'chore(package): Bump version', body: '' })
+            return nextVersion
+          })
+        } else {
+          return nextVersion
+        }
       }).then(nextVersion => {
         return new Promise((resolve, reject) => {
           const messageOptions = commits.map(commit => {
