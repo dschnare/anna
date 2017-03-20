36d3ca771e5cffe2bc45ca6bc46eb796a49fa829
Darren Schnare <schnare.d@gmail.com>
2017-03-20 10:53:54 -0400

    tools(*): Fix exec function so that stderr is trimmed, fix getCommit so that it by default ignores merge commits

--- tools/lib/exec.js
+++ tools/lib/exec.js
@@ -3,8 +3,8 @@ const { exec } = require('child_process')
 module.exports = function (cmd, { cwd } = {}) {
   return new Promise((resolve, reject) => {
     exec(cmd, { cwd: cwd || process.cwd() }, (error, stdout, stderr) => {
-      if (stderr) {
-        reject(new Error(stderr))
+      if (stderr.trim()) {
+        reject(new Error(stderr.trim()))
       } else if (error) {
         reject(error)
       } else {
--- tools/lib/getCommits.js
+++ tools/lib/getCommits.js
@@ -1,12 +1,13 @@
 const exec = require('./exec')
 const getLatestTag = require('./getLatestTag')
 
-module.exports = function getCommits (refBegin = '@latest-tag', refEnd = 'HEAD') {
+module.exports = function getCommits (refBegin = '@latest-tag', refEnd = 'HEAD', { noMerges = true } = {}) {
   return Promise.all([
     refBegin === '@latest-tag' ? getLatestTag() : refBegin,
     refEnd === '@latest-tag' ? getLatestTag() : refEnd
   ]).then(([ refBegin, refEnd ]) => {
-    const cmd = `git log ${refBegin}..${refEnd} --pretty=format:"%s:::%b#END#"`
+    const noMergesOption = noMerges ? '--no-merges' : ''
+    const cmd = `git log ${refBegin}..${refEnd} ${noMergesOption} --pretty=format:"%s:::%b#END#"`
     return exec(cmd).then(stdout => {
       const parts = stdout.trim().replace(/#END#$/, '').split(/:::|#END#/)
       return parts.reduce((commits, part, p) => {
