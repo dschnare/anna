6860c48b115cd158415a044eacce49370ea39777
Darren Schnare <schnare.d@gmail.com>
2017-04-17 12:13:08 -0400

    refactor(*): Refactor all analyzers
    Implement a modular text scanner and accompanying analyzer.
    
    Refactor all analyzers to use the new scanner and analyzer so that
    comments and more complex source text can be correctly analyzed.

--- fixtures/js/pages/home/home.js
+++ fixtures/js/pages/home/home.js
@@ -3,6 +3,23 @@
 var imageUrl = '/home/image.png'
 var bgMusicUrl = "/no-folder/music.mp3"
 
+export default React.createClass({
+  render: function () {
+    // Use asset URLs as they will be served by the server so that the URLs
+    // don't need to be translated.
+    var imageUrl = this.props.imageUrl || '/assets/Badge/default-badge.svg'
+    return (
+      <div className="Badge"><img src={imageUrl} /></div>
+    )
+  }
+})
+
+'/* string */'
+
+'/* string'
+
+'// string '
+
 /* 'some/url/file.video' */
 
 // 'some/file.stuff'
